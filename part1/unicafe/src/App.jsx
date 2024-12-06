bdeb9a0406a26b46996e45db3b391a80ee8c6869
ashdkx <ashdkx@yahoo.com>
2024-12-05 23:47:04 -0500

    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,9 +9,24 @@ const Button = ({ onClick, label }) => {
 }
 
 const Label = (props) => {
+  if (props.text === 'positive') 
+      return (
+        <>
+          {isNaN(props.value) ? <p> {props.text} {'0 %'}</p> : <p> {props.text} {props.value + ' %'}</p>}
+        </>
+      )
+  
   return (
     <>
       <p>{props.text} {props.value}</p>
+    </>
+  )
+}
+
+const Title = (props) => {
+  return (
+    <>
+      <h1>{props.text}</h1>
     </>
   )
 }
@@ -23,14 +38,16 @@ function App() {
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <Title text={'give feedback'} />
       <Button onClick={() => setGood(good + 1)} label={'good'} />
       <Button onClick={() => setNeutral(neutral + 1)} label={'neutral'} />
       <Button onClick={() => setBad(bad + 1)} label={'bad'} />
-      <h1>statistics</h1>
+      <Title text={'statistics'} />
       <Label text={'good'} value={good} />
       <Label text={'neutral'} value={neutral} />
       <Label text={'bad'} value={bad} />
+      <Label text={'total'} value={good + neutral + bad} />
+      <Label text={'positive'} value={good / (good + neutral + bad)*100} />
     </div>
   )
 
