60ccc55d08cb3d9bfdac58e86120d818360e9393
ashdkx <ashdkx@yahoo.com>
2024-12-05 23:53:58 -0500

    exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,13 +9,13 @@ const Button = ({ onClick, label }) => {
 }
 
 const Label = (props) => {
-  if (props.text === 'positive') 
-      return (
-        <>
-          {isNaN(props.value) ? <p> {props.text} {'0 %'}</p> : <p> {props.text} {props.value + ' %'}</p>}
-        </>
-      )
-  
+  if (props.text === 'positive')
+    return (
+      <>
+        {isNaN(props.value) ? <p> {props.text} {'0 %'}</p> : <p> {props.text} {props.value + ' %'}</p>}
+      </>
+    )
+
   return (
     <>
       <p>{props.text} {props.value}</p>
@@ -31,10 +31,28 @@ const Title = (props) => {
   )
 }
 
+const Statistics = ({statistics}) => {
+  return (
+    <>
+      <Title text={'statistics'} />
+      <Label text={'good'} value={statistics.good} />
+      <Label text={'neutral'} value={statistics.neutral} />
+      <Label text={'bad'} value={statistics.bad} />
+      <Label text={'total'} value={statistics.good + statistics.neutral + statistics.bad} />
+      <Label text={'positive'} value={statistics.good / (statistics.good + statistics.neutral + statistics.bad) * 100} />
+    </>
+  )
+}
+
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const statistics = {
+    good: good,
+    neutral: neutral,
+    bad: bad
+  }
 
   return (
     <div>
@@ -42,12 +60,7 @@ function App() {
       <Button onClick={() => setGood(good + 1)} label={'good'} />
       <Button onClick={() => setNeutral(neutral + 1)} label={'neutral'} />
       <Button onClick={() => setBad(bad + 1)} label={'bad'} />
-      <Title text={'statistics'} />
-      <Label text={'good'} value={good} />
-      <Label text={'neutral'} value={neutral} />
-      <Label text={'bad'} value={bad} />
-      <Label text={'total'} value={good + neutral + bad} />
-      <Label text={'positive'} value={good / (good + neutral + bad)*100} />
+      <Statistics statistics={statistics}/>
     </div>
   )
 
