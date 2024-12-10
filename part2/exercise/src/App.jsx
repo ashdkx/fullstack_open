e1614499c623385c27478e0c78d0fa2ff0cec984
ashdkx <ashdkx@yahoo.com>
2024-12-10 01:33:08 -0500

    exercise 2.1

---
+++ part2/exercise/src/App.jsx
@@ -0,0 +1,75 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1> {props.course} </h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part}  {props.exercise}
+      </p>
+    </>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <>
+    {parts.map(part => 
+      <Part part={part.name} exercise={part.exercises}/>
+      )}
+    </>
+  )
+}
+
+const Total = ({parts}) => {
+  return (
+    <>
+      <p>
+        Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0 )}
+      </p>
+    </>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course = {course.course}/>
+      <Content parts = {course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    course: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    { 
+      name: 'State of a component',
+      exercises:14 
+    }
+  ]
+}
+
+  return (
+    <div>
+      <Course course= {course}/>
+    </div>
+  )
+}
+
+export default App
